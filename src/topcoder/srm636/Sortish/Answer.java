f949118209990efef59e6c989d62f4cd79bf8d9f
ralph.joachim <ralph.joachim@docusign.com>
2014-11-03 08:32:45 -0800

    Sortedness finished

--- src/topcoder/srm636/Sortish/Answer.java
+++ src/topcoder/srm636/Sortish/Answer.java
@@ -23,7 +23,7 @@ public class Answer {
         //test = test(test, 4, new int[] { 0, 0, 0, 0 }, 5);
         // All 5 possible ways are: {1, 3, 4, 2}, {1, 4, 2, 3}, {2, 1, 4, 3},
         // {2, 3, 1, 4}, {3, 1, 2, 4}.
-        
+
         // =========================================================================
         // 2)
         //test = test(test, 2, new int[] { 1, 3, 2 }, 1);
@@ -43,24 +43,28 @@ public class Answer {
 
         // =========================================================================
         // 5)
-        //test = test(test, 30, new int[] { 0, 6, 3, 0, 0, 2, 10, 0, 0, 0 }, 34);
+        //est = test(test, 30, new int[] { 0, 6, 3, 0, 0, 2, 10, 0, 0, 0 },
+        //34);
 
         // =========================================================================
         // 6)
 
         test = test(test, 100, new int[] { 0, 13, 0, 0, 12, 0, 0, 0, 2, 0, 0,
-                10, 5, 0, 0, 0, 0, 0, 0, 7, 15, 16, 20 }, 53447326);
+        10, 5, 0, 0, 0, 0, 0, 0, 7, 15, 16, 20 }, 53447326);
 
     }
 
-    private static int test(int test, int sortedness, int[] seq, long expected) throws Exception {
+    private static int test(int test, int sortedness, int[] seq, long expected)
+            throws Exception {
         System.out.println("Test" + (test++));
         long actual = ways(sortedness, seq);
         System.out.println("sortedness: " + sortedness);
         System.out.println("seq: ");
         boolean start = true;
         for (int val : seq) {
-            if(!start){System.out.print(","); }
+            if (!start) {
+                System.out.print(",");
+            }
             System.out.print(val);
             start = false;
         }
@@ -88,14 +92,15 @@ public class Answer {
         }
         return r;
     }
-    
-    private static long ways(int sortedness, int[] seq) throws Exception
-    {
+
+    private static long ways(int sortedness, int[] seq) throws Exception {
         List<Integer> blank = new ArrayList<Integer>();
         List<Integer> missing = new ArrayList<Integer>();
         int n = seq.length;
-        int maxSortedness = ((n-1)*n)/2;
-        if(sortedness>maxSortedness){return 0;}
+        int maxSortedness = ((n - 1) * n) / 2;
+        if (sortedness > maxSortedness) {
+            return 0;
+        }
         for (int i = 0; i < n; i++) {
             if (seq[i] == 0) {
                 blank.add(i);
@@ -103,13 +108,13 @@ public class Answer {
         }
         for (int x = 1; x <= n; x++) {
             boolean found = false;
-            for(int val:seq){
-                if (val==x) {
+            for (int val : seq) {
+                if (val == x) {
                     found = true;
                     break;
                 }
             }
-            if(!found){
+            if (!found) {
                 missing.add(x);
             }
         }
@@ -117,15 +122,18 @@ public class Answer {
         int initialSortedness = 0;
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
-                if ( (seq[i] < seq[j]) && (seq[i] != 0) && (seq[j] != 0) ) {
+                if ((seq[i] < seq[j]) && (seq[i] != 0) && (seq[j] != 0)) {
                     initialSortedness++;
                 }
             }
         }
-        
-        // calculate the sortedness added by choosing a number in each of the blank places
+
+        // calculate the sortedness added by choosing a number in each of the
+        // blank places
         int m = blank.size();
-        if(m<=1){return 1;}
+        if (m <= 1) {
+            return 1;
+        }
         int[][] addSort = new int[m][m];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < m; j++) {
@@ -133,12 +141,12 @@ public class Answer {
                 addSort[i][j] = 0;
                 for (int k = 0; k < n; k++) {
                     if (seq[k] != 0) {
-                        if ( k < blank.get(i) ) {
-                            if ( seq[k] < missing.get(j) ) {
+                        if (k < blank.get(i)) {
+                            if (seq[k] < missing.get(j)) {
                                 addSort[i][j]++;
                             }
                         } else if (k > blank.get(i)) {
-                            if ( seq[k] > missing.get(j) ) {
+                            if (seq[k] > missing.get(j)) {
                                 addSort[i][j]++;
                             }
                         }
@@ -149,10 +157,10 @@ public class Answer {
         // we need to precalculate the sortedness of each permutation.
         // we *could* just use the permutation as a key for an associative array
         // but the following is a neat trick. We know that next_permutation will
-        // always generate all permuations in the same order, so we can assign 
+        // always generate all permuations in the same order, so we can assign
         // an unique id to each permutation by the order it is visited.
-        int[] permScore1 = new int[5040];       
-        int[]  per1 = new int[m / 2];
+        int[] permScore1 = new int[5040];
+        int[] per1 = new int[m / 2];
         for (int i = 0; i < m / 2; i++) {
             per1[i] = i;
         }
@@ -162,44 +170,44 @@ public class Answer {
         }
         do {
             permScore1[pid++] = getSortedness(per1);
-        } while ( next_permutation(per1) );
+        } while (next_permutation(per1));
 
         // An issue is that the number of missing elements might be odd, so
         // the number of small and large elements might be different so we might
         // need to repeat for large numbers:
         int[] permScore2 = new int[5040];
-        int[] per2 = new int[(m+1) / 2];
-        for (int i = 0; i < (m+1) / 2; i++) {
+        int[] per2 = new int[(m + 1) / 2];
+        for (int i = 0; i < (m + 1) / 2; i++) {
             per2[i] = i;
         }
         pid = 0;
         do {
             permScore2[pid++] = getSortedness(per2);
-        } while ( next_permutation(per2) );
-
+        } while (next_permutation(per2));
 
         long res = 0;
         // we can generate all combinations using next_permutation. Okay, this
         // solution is very next_permutation-specific, I admit.
         int[] comb = new int[m];
-        for (int i = 0; i < m/2; i++) {
+        for (int i = 0; i < m / 2; i++) {
             comb[i] = 0;
         }
-        for (int j = m/2; j < m; j++) {
+        for (int j = m / 2; j < m; j++) {
             comb[j] = 1;
         }
         int comboNum = 0;
-        Map<String,Boolean> seen = new HashMap<String,Boolean>();
+        Map<String, Boolean> seen = new HashMap<String, Boolean>();
         do {
-            //System.out.println("------------------------");
-            //System.out.print("Combo: ");
-            //print(comb);
-            int MAX = 1999*7; //The maximum value of s. Roughly you can
+//             System.out.println("------------------------");
+//             System.out.print("Combo: ");
+//             print(comb);
+            int MAX = 1999 * 7; // The maximum value of s. Roughly you can
             // imagine that for each of the m/2 small numbers we might add less
             // than n-1 sortedness. The real limit is smaller but is not needed
             // to find a tight bound.
 
-            int combsort = getSortedness(comb); //sortedness by this combination
+            int combsort = getSortedness(comb); // sortedness by this
+                                                // combination
 
             // small : For each small permutation, find s and increment small[s]
             long[] small = new long[MAX + 1];
@@ -207,105 +215,97 @@ public class Answer {
             for (int i = 0; i < m / 2; i++) {
                 per1[i] = i;
             }
-            int[] copy = new int[seq.length];
+
             do {
                 int s = permScore1[pid++];
                 int j = 0;
                 for (int i = 0; i < m; i++) {
                     if (comb[i] == 0) {
                         int missingIndex = per1[j++];
-                        copy[blank.get(i)] = missing.get(missingIndex);                        
-                        s += addSort[i][ missingIndex ];
+                        s += addSort[i][missingIndex];
                     }
                 }
                 small[s]++;
-            } while ( next_permutation(per1) );
+            } while (next_permutation(per1));
 
             // large : For each large permutation, find the required s for a
             // matching small permutation and add small[s] to res
             pid = 0;
-            for (int i = 0; i < (m+1) / 2; i++) {
+            for (int i = 0; i < (m + 1) / 2; i++) {
                 per2[i] = i;
             }
             do {
                 int s = permScore2[pid++];
                 int j = 0;
-                
+
                 for (int i = 0; i < m; i++) {
                     if (comb[i] == 1) {
-                        int missingIndex = per2[j++] + (m/2);
-                        copy[blank.get(i)] = missing.get(missingIndex);
-                        s += addSort[i][ missingIndex ];
+                        int missingIndex = per2[j++] + (m / 2);
+                    
+                        s += addSort[i][missingIndex];
                     }
                 }
-                String str = "";
-                for(int v:copy){
-                    str+=v;
-                }
-                if(seen.containsKey(str)){
-                    continue;
-                }
-                else{
-                    seen.put(str, true);
-                }
+
                 int ws = sortedness - initialSortedness - s - combsort;
-                
-                if ( 0 <= ws && ws <= MAX) {
+
+                if (0 <= ws && ws <= MAX) {
                     res += small[ws];
                 }
-                //System.out.print("seq: ");
-                //print(seq);
-                //System.out.println("combo["+comboNum+"]: remaining="+ws + " res:"+res);
+//                 System.out.print("seq: ");
+//                 print(seq);
+//                 System.out.println("combo["+comboNum+"]: remaining="+ws +
+//                 " res:"+res);
                 ++comboNum;
-            } while ( next_permutation(per2) );
-            
-        } while (next_permutation(comb));
+            } while (next_permutation(per2));
 
+        } while (next_permutation(comb, 1));
 
         return res;
-    }    
+    }
 
-    private static Map<int[],int[]> origHash = new HashMap<int[],int[]>();
-    private static Map<int[],int[]> indexHash = new HashMap<int[],int[]>();
     private static boolean next_permutation(int[] input) {
+        int max = input.length - 1;
+        return next_permutation(input, max);
+    }
+
+    private static boolean next_permutation(int[] input, int max) {
         int n = input.length;
-        int max = n - 1;
+
         
-        int[] indices = getIndices(input);
-        int[] original = getOrig(input);
-        
-        boolean lastPerm = true;
-        int x = n - 1;
-        for (int j = 0; j < n; j++) {
-            int v = indices[j];
-            if (v != x) {
+        if(input[0]==max){
+            boolean lastPerm = true;
+        int prev = max;        
+        for (int j = 1; j < n; j++) {
+            int v = input[j];
+            if (v != prev && v!=(prev-1)) {
                 lastPerm = false;
                 break;
             }
-            --x;
-        }
-        
-        if(lastPerm) {
-            indexHash.remove(input);
-            origHash.remove(input);
-            return false; 
+            prev = v;
         }
 
-        Map<Integer, Boolean> fixed = new HashMap<Integer, Boolean>();
+        if (lastPerm) {
+            return false;
+        }
+        }
+           
+        
+
+        Map<Integer, Integer> fixed = new HashMap<Integer, Integer>();
         for (int i = 0; i < n; i++) {
-            int v = indices[i];
-            fixed.put(v, true);
+            int v = input[i];
+            addToFixed(fixed, v);
         }
 
         List<Integer> remaining = new ArrayList<Integer>();
 
         for (int current = n - 1; current >= 0; --current) {
-            int currentVal = indices[current];
+            int currentVal = input[current];
             if (current == n - 1) {
                 if (fixed.containsKey(currentVal)) {
                     addToRemaining(fixed, remaining, currentVal);
                 }
-            } else if (canIncrement(max, current, fixed, indices, remaining)) {
+            } else if (canIncrement(max, current, fixed, input, remaining)) {
                 break;
             } else {
                 if (fixed.containsKey(currentVal)) {
@@ -313,54 +313,51 @@ public class Answer {
                 }
             }
         }
-        
-        indexHash.put(input, indices);
-        for(int k=0; k<n; k++){
-            input[k] = original[indices[k]];
-        }
-                
+
         return true;
     }
 
-    private static int[] getIndices(int[] input) {
-        if(indexHash.containsKey(input)){
-            return indexHash.get(input);
-        }
-        else{
-            int n = input.length;
-            int[] indices = new int[n];
-            for(int i=0; i<n; i++){
-                indices[i] = i;
-            }
-            indexHash.put(input, indices);
-            return indices;
+    private static void addToFixed(Map<Integer, Integer> fixed, int v) {
+        if (fixed.containsKey(v)) {
+            fixed.put(v, fixed.get(v) + 1);
+        } else {
+            fixed.put(v, 1);
         }
     }
 
-    private static int[] getOrig(int[] input) {
-        if(origHash.containsKey(input)){
-            return origHash.get(input);
-        }
-        else{
-            int n = input.length;
-            int[] orig = new int[n];
-            for(int i=0; i<n; i++){
-                orig[i] = input[i];
-            }
-            origHash.put(input, orig);
-            return orig;
-        }
-    }
-
-    private static void addToRemaining(Map<Integer, Boolean> fixed,
+    private static void addToRemaining(Map<Integer, Integer> fixed,
             List<Integer> remaining, int currentVal) {
-        fixed.remove(currentVal);
-        remaining.add(currentVal);
-        Collections.sort(remaining);
+        if (fixed.containsKey(currentVal)) {
+            int count = fixed.get(currentVal);
+            --count;
+            if (count == 0) {
+                fixed.remove(currentVal);
+            } else {
+                fixed.put(currentVal, count);
+            }
+        }
+        // remaining.add(currentVal);
+        // Collections.sort(remaining);
+
+        int n = remaining.size();
+        if (n == 0) {
+            remaining.add(currentVal);
+        } else {
+            int insertAt = n;
+            for (int i = 0; i < remaining.size(); i++) {
+                int thisVal = remaining.get(i);
+                if (currentVal < thisVal) {
+                    insertAt = i;
+                    break;
+                }
+            }
+            remaining.add(insertAt,currentVal);
+        }
+
     }
 
     private static boolean canIncrement(int max, int current,
-            Map<Integer, Boolean> fixed, int[] input, List<Integer> remaining) {
+            Map<Integer, Integer> fixed, int[] input, List<Integer> remaining) {
         int currentVal = input[current];
         if (currentVal == max || current == input.length - 1) {
             return false;
@@ -370,14 +367,15 @@ public class Answer {
             return false;
         }
 
-        boolean  found = false;
+        boolean found = false;
         for (int k = 0; k < remaining.size(); k++) {
             int newVal = remaining.get(k);
             if (currentVal < newVal) {
                 found = true;
                 remaining.remove(k);
                 addToRemaining(fixed, remaining, currentVal);
-                fixed.put(newVal, true);
+                //fixed.put(newVal, true);
+                addToFixed(fixed,newVal);
                 input[current] = newVal;
                 break;
             }
@@ -390,14 +388,15 @@ public class Answer {
         for (int index = current + 1; index < input.length; index++) {
             int otherVal = remaining.get(0);
             remaining.remove(0);
-            fixed.put(otherVal, true);
+            //fixed.put(otherVal, true);
+            addToFixed(fixed,otherVal);
             input[index] = otherVal;
         }
         return true;
     }
-    
+
     private static void print(int[] elems) {
-        for(int v:elems){
+        for (int v : elems) {
             System.out.print(v + " ");
         }
         System.out.println();
